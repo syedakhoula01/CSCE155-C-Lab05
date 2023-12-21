9d930786219ff9b04c9e68cab63ef6d8370789b7
syedakhoula01 <syeda.khoula.abid234@gmail.com>
2023-12-21 04:23:12 -0800

    min function

--- colorUtils.c
+++ colorUtils.c
@@ -15,3 +15,24 @@ int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
+
+//min function
+
+int min(int a,int b, int c)
+{
+if(a<=b && a<=c)
+{
+  return a;
+}
+else if(b<=a &&b<=c)
+{
+return b;
+}
+else
+{
+  return c;
+}
+}
+
+
+
